34408f428e74b0469ba74c55545eeafd208b41ff
Muhammad Rafly Abdillah <150163424+mraflya1204@users.noreply.github.com>
2024-11-26 21:06:24 +0700

    Added some user interaction

--- src/timeline/GraphExec.java
+++ src/timeline/GraphExec.java
@@ -1,14 +1,45 @@
 package timeline;
 
+import java.util.Scanner;
+
 public class GraphExec {
     public static void main(String[] args) {
-        GraphBuilder graphBuilder = new GraphBuilder();
-
         String fileName = "E:\\Programs\\Others\\Java\\Workspace\\Tegraf_FP\\input.tsv";
-        graphBuilder.loadGraphFromFile(fileName);
-        graphBuilder.addEdges();
-        graphBuilder.findMST();
         
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("1. Abort Operation\n2. View Feed Only\n3. Add Feed\nEnter mode: ");
+        int mode = myScanner.nextInt();
+        myScanner.nextLine();
         
+        if(mode == 1) {
+        	myScanner.close();
+        	System.out.println("\nOperation aborted");
+        }
+        else if(mode == 2) {
+        	myScanner.close();
+        	System.out.println();
+            GraphBuilder graphBuilder = new GraphBuilder();
+            graphBuilder.loadGraphFromFile(fileName);
+            graphBuilder.addEdges();
+            graphBuilder.findMST();
+        }
+        else if(mode == 3) {
+            System.out.print("Input your tweet: ");
+            String newText = myScanner.nextLine();
+            myScanner.close();
+            System.out.println();
+            System.out.println(newText);
+            // Proceed with graph operations
+            GraphBuilder graphBuilder = new GraphBuilder();
+            graphBuilder.loadGraphFromFile(fileName);
+            graphBuilder.addEdges();
+            graphBuilder.findMST();
+            
+            
+            FileAppender.appendToFile(fileName, newText);
+        }
+        else {
+            System.out.println("\nInvalid input, operation aborted");
+        }
     }
 }
